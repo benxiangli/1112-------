e21d0f44393aad736338925e680bab2666cf5315
xiang <xiang20000214@gmail.com>
2023-03-19 15:17:23 +0800

    edit bat

--- run.bat
+++ run.bat
@@ -1,5 +1,6 @@
+REM @echo off
 cd /d C:\Users\xiang\Desktop\1112航測資料\1112航測成績辨識君
 call C:/Users/xiang/anaconda3/Scripts/activate
 call conda activate pywebcrawler
 call python identify.py
-REM pause
+pause
